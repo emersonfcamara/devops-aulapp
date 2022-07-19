f8a8ffd108472c2371b58d8fc1caa42a698cbbaa
emersonferreira <emerson.ferreira@fcamara.com.br>
2022-07-19 17:12:24 -0300

    Disabling the SSL for MySQL DB.

--- terraform/main.tf
+++ terraform/main.tf
@@ -185,6 +185,11 @@ resource "azurerm_frontdoor" "fd_aulapp" {
     }
   }
 
+  backend_pool_settings {
+    backend_pools_send_receive_timeout_seconds   = 0
+    enforce_backend_pools_certificate_name_check = false
+  }
+
   backend_pool_load_balancing {
     name = "${var.fd_aulapp_bplb}"
   }
